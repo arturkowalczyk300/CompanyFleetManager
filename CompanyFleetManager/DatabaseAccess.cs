57dabe3e848501996c1917a8152e37ff1ebc23df
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-08-23 23:25:03 +0200

    add basic database create and access code

--- CompanyFleetManager/DatabaseAccess.cs
+++ CompanyFleetManager/DatabaseAccess.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CompanyFleetManager.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,73 @@ using System.Threading.Tasks;
 
 namespace CompanyFleetManager
 {
-    internal class DatabaseAccess
+    public class DatabaseAccess
     {
+        private DatabaseContext dbContext = new DatabaseContext();
+
+        public DatabaseAccess() { }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            dbContext.Vehicles.Add(vehicle);
+            dbContext.SaveChanges();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            dbContext.Employees.Add(employee);
+            dbContext.SaveChanges();
+        }
+
+        public void AddRental(Rental rental)
+        {
+            dbContext.Rentals.Add(rental);
+            dbContext.SaveChanges();
+        }
+
+        public List<Vehicle> GetVehicles() => dbContext.Vehicles.ToList();
+
+        public List<Employee> GetEmployees() => dbContext.Employees.ToList();
+
+        public List<Rental> GetRentals() => dbContext.Rentals.ToList();
+
+        public void DeleteVehicle(Vehicle vehicle)
+        {
+            dbContext.Vehicles.Remove(vehicle);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteAllVehicles()
+        {
+            var all = from vehicles in dbContext.Vehicles select vehicles;
+            dbContext.Vehicles.RemoveRange(all);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteAllEmployees()
+        {
+            var all = from employees in dbContext.Employees select employees;
+            dbContext.Employees.RemoveRange(all);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteAllRentals()
+        {
+            var all = from rentals in dbContext.Rentals select rentals;
+            dbContext.Rentals.RemoveRange(all);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteRentals(Rental rental)
+        {
+            dbContext.Rentals.Remove(rental);
+            dbContext.SaveChanges();
+        }
     }
 }
--- CompanyFleetManager/DatabaseContext.cs
+++ CompanyFleetManager/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CompanyFleetManager.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace CompanyFleetManager
 {
-    internal class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+        public string DbPath { get; }
+
+        public DatabaseContext()
+        {
+            DbPath = "fleet.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data source={DbPath}");
+        }
     }
 }
