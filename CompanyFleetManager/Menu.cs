98baf9de8034929c4d69a1ed0525b3baa0dc577e
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-08-23 18:15:21 +0200

    add user menu

--- CompanyFleetManager/Menu.cs
+++ CompanyFleetManager/Menu.cs
@@ -8,5 +8,25 @@ namespace CompanyFleetManager
 {
     internal class Menu
     {
+        private Dictionary<string, Program.MenuCallback> Actions;
+
+        public Menu(Dictionary<string, Program.MenuCallback> actions)
+        {
+            Actions = actions;
+        }
+
+        public void Handle()
+        {
+            Console.WriteLine("Select an option: ");
+            Console.WriteLine("\t1. Check if database has been created");
+            var choice = Console.ReadLine();
+
+            if (choice != null && Actions.ContainsKey(choice))
+            {
+                Actions[choice].Invoke();
+            }
+            else
+                Console.WriteLine("Invalid option!");
+        }
     }
 }
--- CompanyFleetManager/Program.cs
+++ CompanyFleetManager/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        public delegate void MenuCallback();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var actions = new Dictionary<string, MenuCallback>()
+            {
+                { "1", () => { Console.WriteLine("First option launched!");} }
+            };
+
+            var menu = new Menu(actions);
+
+            while (true)
+            {
+                menu.Handle();
+            }
         }
     }
 }
