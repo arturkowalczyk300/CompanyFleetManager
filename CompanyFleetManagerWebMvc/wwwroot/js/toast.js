5b13675e37b7fd18ef34256054d35164f1eb6199
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-09-17 20:54:59 +0200

    add function to display short toast message

---
+++ CompanyFleetManagerWebMvc/wwwroot/js/toast.js
@@ -0,0 +1,8 @@
+﻿function showToast(message) {
+    var toast = document.getElementById("toast");
+    toast.className = "show";
+    toast.innerHTML = message;
+    setTimeout(function () {
+        toast.className = toast.className.replace("show", "");
+    }, 3000);
+}
