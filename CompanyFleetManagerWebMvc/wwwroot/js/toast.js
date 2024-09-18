2b46e98783b676a4bf48a27a4d9e7e5b81cfb81c
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-09-18 18:01:05 +0200

    add short alert message about becoming logged in/logged out

--- CompanyFleetManagerWebMvc/wwwroot/js/toast.js
+++ CompanyFleetManagerWebMvc/wwwroot/js/toast.js
@@ -1,8 +1,18 @@
-﻿function showToast(message) {
+﻿const TOAST_DURATION_MS = 2000;
+
+function showToast(message) {
     var toast = document.getElementById("toast");
     toast.className = "show";
     toast.innerHTML = message;
     setTimeout(function () {
         toast.className = toast.className.replace("show", "");
-    }, 3000);
+    }, TOAST_DURATION_MS);
+}
+
+function showToastThenSubmit(message, formId) {
+    showToast(message);
+
+    setTimeout(function () {
+        document.getElementById(formId).submit();
+    }, TOAST_DURATION_MS);
 }
