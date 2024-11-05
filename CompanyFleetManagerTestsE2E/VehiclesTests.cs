a7e14c6d441d814784b224ff64f3fe144a137bbf
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-11-06 00:05:43 +0100

    add end-to-end tests for the Vehicles section: loading entities and adding new ones

--- CompanyFleetManagerTestsE2E/VehiclesTests.cs
+++ CompanyFleetManagerTestsE2E/VehiclesTests.cs
@@ -3,26 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 
 namespace CompanyFleetManagerTestsE2E
 {
-    public class VehiclesTests
+    public class VehiclesTests : IAsyncLifetime
     {
-        public VehiclesTests()
-        {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
 
+        public async Task InitializeAsync()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _page = await _browser.NewPageAsync();
+            await _page.GotoAsync("https://localhost:7079");
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
         }
 
         [Fact]
-        public void VehiclesTest_OnLoad_ShouldDisplayVehicles()
+        public async void VehiclesTest_OnLoad_ShouldDisplayVehicles()
         {
-            throw new NotImplementedException();
+            await _page.ClickAsync("#PageVehicles");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+
+            Assert.NotEmpty(rows);
+
+            var firstRowCells = await rows.First().QuerySelectorAllAsync("td");
+            var modelText = await firstRowCells[1].InnerTextAsync();
+            var licencePlateText = await firstRowCells[2].InnerTextAsync();
+
+            Assert.Equal("A6", modelText);
+            Assert.Equal("DW 321AB", licencePlateText);
         }
 
         [Fact]
-        public void VehiclesTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
+        public async void VehiclesTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
         {
-            throw new NotImplementedException();
+            await _page.ClickAsync("#PageVehicles");
+
+            await _page.ClickAsync("a[href='/Vehicles/Create']");
+
+            await _page.FillAsync("input[name='Brand']", "Fiat");
+            await _page.FillAsync("input[name='Model']", "Ducato");
+            await _page.FillAsync("input[name='LicencePlateNumber']", "WE XA023");
+            await _page.FillAsync("input[name='ProductionYear']", "2021");
+            await _page.FillAsync("input[name='Mileage']", "123016");
+            await _page.FillAsync("input[name='VehicleInspectionValidity']", "01.11.2025");
+            await _page.CheckAsync("input[name='IsDamaged']");
+
+            await _page.ClickAsync("input[type='submit']");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+
+            Assert.NotEmpty(rows);
+
+            bool vehicleFound = false;
+            foreach (var row in rows)
+            {
+                var cells = await row.QuerySelectorAllAsync("td");
+                var modelText = await cells[1].InnerTextAsync();
+                var licencePlateText = await cells[2].InnerTextAsync();
+                if (modelText == "Ducato" && licencePlateText == "WE XA023")
+                {
+                    vehicleFound = true;
+                    break;
+                }
+            }
+
+            Assert.True(vehicleFound);
         }
 
         [Fact]
