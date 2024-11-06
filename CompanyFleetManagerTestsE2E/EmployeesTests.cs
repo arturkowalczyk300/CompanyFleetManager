c38645f387517edbe168a4717f3209baa5d3d970
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-11-06 08:45:39 +0100

    add e2e tests for the Employees and Rental sections: loading entities and adding new ones

--- CompanyFleetManagerTestsE2E/EmployeesTests.cs
+++ CompanyFleetManagerTestsE2E/EmployeesTests.cs
@@ -1,17 +1,79 @@
-﻿namespace CompanyFleetManagerTestsE2E
+﻿using Microsoft.Playwright;
+
+namespace CompanyFleetManagerTestsE2E
 {
-    public class EmployeesTests
+    public class EmployeesTests : IAsyncLifetime
     {
-        [Fact]
-        public void EmployeesTest_OnLoad_ShouldDisplayEmployees()
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
+
+        public async Task InitializeAsync()
         {
-            throw new NotImplementedException();
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _page = await _browser.NewPageAsync();
+            await _page.GotoAsync("https://localhost:7079");
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
         }
 
         [Fact]
-        public void EmployeesTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
+        public async Task EmployeesTest_OnLoad_ShouldDisplayEmployees()
         {
-            throw new NotImplementedException();
+            await _page.ClickAsync("#PageEmployees");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+            Assert.NotEmpty(rows);
+
+            var firstRowCells = await rows.First().QuerySelectorAllAsync("td");
+            var occupationText = await firstRowCells[0].InnerTextAsync();
+            var forenameText = await firstRowCells[2].InnerTextAsync();
+
+            Assert.Equal("Driver", occupationText);
+            Assert.Equal("Janusz", forenameText);
+        }
+
+        [Fact]
+        public async Task EmployeesTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
+        {
+            await _page.ClickAsync("#PageEmployees");
+            await _page.ClickAsync("a[href='/Employees/Create']");
+
+            await _page.FillAsync("input[name='Occupation']", "Technician");
+            await _page.FillAsync("input[name='Address']", "Oława ul. Wiśniowa 12/3");
+            await _page.FillAsync("input[name='Forename']", "Jakub");
+            await _page.FillAsync("input[name='Middlename']", "Mariusz");
+            await _page.FillAsync("input[name='Surname']", "Kowalski");
+            await _page.FillAsync("input[name='NationalIdentityNumber']", "837174628");
+            await _page.FillAsync("input[name='WorkPhoneNumber']", "874123084");
+            await _page.FillAsync("input[name='PrivatePhoneNumber']", "432503931");
+            await _page.FillAsync("input[name='DrivingLicenseCategories']", "B");
+            await _page.FillAsync("input[name='DrivingLicenseValidity']", "3.10.2035");
+            await _page.FillAsync("input[name='HiredUntil']", "31.10.2025");
+            await _page.ClickAsync("input[type='submit']");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+            Assert.NotEmpty(rows);
+
+            bool employeeFound = false;
+            foreach (var row in rows)
+            {
+                var cells = await row.QuerySelectorAllAsync("td");
+                var occupationText = await cells[0].InnerTextAsync();
+                var forenameText = await cells[2].InnerTextAsync();
+                if (occupationText == "Technician" && forenameText == "Jakub")
+                {
+                    employeeFound = true;
+                    break;
+                }
+            }
+
+            Assert.True(employeeFound);
         }
 
         [Fact]
--- CompanyFleetManagerTestsE2E/RentalsTests.cs
+++ CompanyFleetManagerTestsE2E/RentalsTests.cs
@@ -1,22 +1,72 @@
-﻿namespace CompanyFleetManagerTestsE2E
+﻿using Microsoft.Playwright;
+
+namespace CompanyFleetManagerTestsE2E
 {
-    public class RentalsTests
+    public class RentalsTests : IAsyncLifetime
     {
-        public RentalsTests()
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
+
+        public async Task InitializeAsync()
         {
-           
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _page = await _browser.NewPageAsync();
+            await _page.GotoAsync("https://localhost:7079");
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
         }
 
         [Fact]
-        public void RentalsTest_OnLoad_ShouldDisplayRentals()
+        public async Task RentalsTest_OnLoad_ShouldDisplayRentals()
         {
-            throw new NotImplementedException();
+            await _page.ClickAsync("#PageRentals");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+            Assert.NotEmpty(rows);
+
+            var firstRowCells = await rows.First().QuerySelectorAllAsync("td");
+            var rentedVehicleText = await firstRowCells[0].InnerTextAsync();
+            var rentingEmployeeText = await firstRowCells[1].InnerTextAsync();
+
+            Assert.Equal("Audi A6, DW 321AB", rentedVehicleText);
+            Assert.Equal("Janusz Mariusz Kowalski, Driver", rentingEmployeeText);
         }
 
         [Fact]
-        public void RentalsTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
+        public async Task RentalsTest_ClickButtonAddAndFillingForm_ShouldCauseAddingNewRecord()
         {
-            throw new NotImplementedException();
+            await _page.ClickAsync("#PageRentals");
+            await _page.ClickAsync("a[href='/Rentals/Create']");
+
+            await _page.SelectOptionAsync("#selectVehicle", new[] { "Skoda Fabia, WE 31DA3" });
+            await _page.SelectOptionAsync("#selectEmployee", new[] { "Andrzej Roman Dobrzański" });
+            await _page.FillAsync("input[name='RentalDate']", "01.11.2024");
+            await _page.FillAsync("input[name='PlannedReturningDate']", "2024-11-11T00:00");
+            await _page.ClickAsync("input[type='submit']");
+
+            var rows = await _page.QuerySelectorAllAsync("table.table tbody tr");
+            Assert.NotEmpty(rows);
+
+            bool rentalFound = false;
+            foreach (var row in rows)
+            {
+                var cells = await row.QuerySelectorAllAsync("td");
+                var rentedVehicleText = await cells[0].InnerTextAsync();
+                var rentingEmployeeText = await cells[1].InnerTextAsync();
+                if (rentedVehicleText == "Skoda Fabia, WE 31DA3" && rentingEmployeeText == "Andrzej Roman Dobrzański, Manager")
+                {
+                    rentalFound = true;
+                    break;
+                }
+            }
+
+            Assert.True(rentalFound);
         }
 
         [Fact]
