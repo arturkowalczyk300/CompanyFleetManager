4c304950f493e0d3bfb2cefd59b2d4f2cb19f992
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-11-21 08:03:30 +0100

    core project: create controllers files structure

---
+++ CompanyFleetManager/Controllers/FleetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyFleetManager.Controllers
+{
+    [ApiController]
+    [Route("api/fleet")]
+    public class FleetController : ControllerBase
+    {
+        private readonly FleetDatabaseAccess _dbAccess;
+
+        public FleetController(FleetDatabaseAccess dbAccess)
+        {
+            _dbAccess = dbAccess;
+        }
+    }
+}
---
+++ CompanyFleetManager/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyFleetManager.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersDatabaseAccess _dbAccess;
+
+        public UsersController(UsersDatabaseAccess dbAccess)
+        {
+            _dbAccess = dbAccess;
+        }
+    }
+}
